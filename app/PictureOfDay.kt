22a42c615dd100dc2987dd450cadfeafb5aa83fa
ahmedsalah <ahmedkamar10@gmail.com>
2022-09-07 23:21:49 +0200

    Remove files

--- app/PictureOfDay.kt
+++
@@ -1,9 +0,0 @@
-package com.udacity.asteroidradar
-
-import com.squareup.moshi.Json
-
-data class pictureOfTheDay(
-    @Json(name = "media_type")
-    val mediaType: String,
-    val title: String,
-    val url: String)
